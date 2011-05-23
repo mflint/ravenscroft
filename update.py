36d3cb90505ebb8bcd804e519cb28f2304fecd84
Matthew Flint <m@tthew.org>
2011-05-24 00:07:26 +0100

    changed to use get_iplayer, and hard-coded 'Any Questions' stuff

--- update.py
+++ update.py
@@ -17,45 +17,28 @@ from BeautifulSoup import BeautifulSoup
 sys.path.append('PyRSS2Gen')
 import PyRSS2Gen
 
-download_directory = "downloaded"
+# TODO: MKF
+download_directory = "/var/www/podcasts/"
 
-podcast_title = "Tom Ravenscroft on BBC Radio 6"
-podcast_description = "An unofficial podcast for Tom Ravenscroft's show on BBC Radio 6, which annoyingly is only available through iPlayer"
-show_url = "http://www.bbc.co.uk/programmes/b00slvl3"
+# TODO: MKF
+podcast_title = "Any Questions"
+# TODO: MKF
+podcast_description = "Topical discussion in which a panel of personalities from the worlds of politics, media and elsewhere are posed questions by the audience."
+# TODO: MKF
+show_url = "http://www.bbc.co.uk/programmes/b006qgvj"
 def episode_description(f):
     # Could examine the file's ID3 tags or extract the date
     # from the filename to make this more interesting
-    return "An episode of Tom Ravenscroft's show on BBC Radio 6"
-
-number_to_keep = None
-base_podcast_url = None
-
-try:
-    if len(sys.argv) != 3:
-        raise Exception, "Wrong number of arguments"
-    number_to_keep = int(sys.argv[1])
-    base_podcast_url = sys.argv[2]
-except Exception as e:
-    print >> sys.stderr, str(e)
-    print >> sys.stderr, "Usage: %s <NUMBER-TO-KEEP> <BASE_PODCAST_URL>"
-    sys.exit(1)
+    return ""
 
 def is_mp3(f):
     return re.search('\.mp3$',f,re.I)
 
-files = [ os.path.join(download_directory,x) for x in os.listdir(download_directory) if is_mp3(x) ]
+def is_aac(f):
+    return re.search('\.aac$',f,re.I)
 
 def mtime(f):
     return datetime.datetime.fromtimestamp(os.stat(f)[stat.ST_MTIME])
-
-files.sort( key = mtime )
-
-for f in files[0:-number_to_keep]:
-    os.remove(f)
-
-opener = urllib2.build_opener()
-
-soup = BeautifulSoup(opener.open(show_url))
 
 def iplayer_console_tag(x):
     if x.name != 'a':
@@ -66,30 +49,22 @@ def iplayer_console_tag(x):
                 return True
     return False
 
-a = soup.find( iplayer_console_tag )
-
-check_call(["mkdir","-p",download_directory])
-
 def iplayer_dl(url):
-    command = [ "ruby", "-I", "iplayer-dl/lib", "iplayer-dl/bin/iplayer-dl",
-                "-d", download_directory, url ]
+#    command = [ "ruby", "-I", "iplayer-dl/lib", "iplayer-dl/bin/iplayer-dl",
+#                "-t", "flashaaclow",
+#                "-d", download_directory, url ]
+    command = [ "../get_iplayer/get_iplayer", url,
+                "--modes=flashaaclow,flashaacstd",
+                "--outputradio", download_directory,
+                "--file-prefix", "<name>-<dldate>",
+                "--command", "\"\"",
+                "--force" ]
+    print >> sys.stdout, command
     check_call(command)
-
-iplayer_dl(a['href'])
-
-# Now generate the XML for the podcast:
-
-files = [ x for x in os.listdir(download_directory) if is_mp3(x) ]
-files.sort( key = lambda x: mtime(os.path.join(download_directory,x)) )
-
-# Make them readable:
-for f in files:
-    time.sleep(2)
-    os.chmod(os.path.join(download_directory,f),file_mode)
 
 def item_from_file(f):
     full = os.path.join(download_directory,f)
-    extension_removed = re.sub('\.mp3$','',f,re.I)
+    extension_removed = re.sub('\.aac$','',f,re.I)
     url_for_mp3 = base_podcast_url + urllib2.quote(f)
     return PyRSS2Gen.RSSItem(
         title = extension_removed,
@@ -99,7 +74,55 @@ def item_from_file(f):
         pubDate = mtime(full),
         enclosure = PyRSS2Gen.Enclosure(url_for_mp3,
                                         os.path.getsize(full),
-                                        "audio/mpeg"))
+                                        "audio/x-aac"))
+
+
+
+
+
+#number_to_keep = None
+number_to_keep = 9999
+base_podcast_url = None
+
+try:
+#    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
+        raise Exception, "Wrong number of arguments"
+#    number_to_keep = int(sys.argv[1])
+#    base_podcast_url = sys.argv[2]
+    base_podcast_url = sys.argv[1]
+except Exception as e:
+    print >> sys.stderr, str(e)
+#    print >> sys.stderr, "Usage: %s <NUMBER-TO-KEEP> <BASE_PODCAST_URL>"
+    print >> sys.stderr, "Usage: %s <BASE_PODCAST_URL>"
+    sys.exit(1)
+
+files = [ os.path.join(download_directory,x) for x in os.listdir(download_directory) if is_aac(x) ]
+
+files.sort( key = mtime )
+
+for f in files[0:-number_to_keep]:
+    os.remove(f)
+
+opener = urllib2.build_opener()
+
+soup = BeautifulSoup(opener.open(show_url))
+
+a = soup.find( iplayer_console_tag )
+
+check_call(["mkdir","-p",download_directory])
+
+iplayer_dl(a['href'])
+
+# Now generate the XML for the podcast:
+
+files = [ x for x in os.listdir(download_directory) if is_aac(x) ]
+files.sort( key = lambda x: mtime(os.path.join(download_directory,x)) )
+
+# Make them readable:
+for f in files:
+    time.sleep(2)
+    os.chmod(os.path.join(download_directory,f),file_mode)
 
 rss = PyRSS2Gen.RSS2(
     title = podcast_title,
@@ -108,6 +131,7 @@ rss = PyRSS2Gen.RSS2(
     lastBuildDate = datetime.datetime.now(),
     items = [ item_from_file(f) for f in files ] )
 
-output_filename = os.path.join(download_directory,"podcast.xml")
+# TODO: MKF
+output_filename = os.path.join(download_directory,"anyquestions.xml")
 rss.write_xml(open(output_filename,"w"))
 os.chmod(output_filename,file_mode)
